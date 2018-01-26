75eafbf5aa5473f4d09ace92ba5c0408eaa7fc29
Ivan Nikiforov <nikiforr@gmail.com>
2018-01-27 00:08:50 +0300

    Add project files.

---
+++ IKitchen/Controllers/FridgeController.cs
@@ -0,0 +1,43 @@
+﻿using IKitchen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IKitchen.Controllers
+{
+    public class FridgeController : Controller
+    {
+        // GET: Fridge
+        public ActionResult Fridge()
+        {
+            var product = new Product {
+                Name = "Apple", Amount = 2, DateTimeBought = DateTime.Today, ExpiryDate = new DateTime(2018, 01, 28), MeasureType ="pcs" 
+            };
+            return View(product);
+        }
+
+        public ActionResult All()
+        {
+            var list = new List<Product>();
+            list.Add(new Product
+            {
+                Name = "Apple",
+                Amount = 2,
+                DateTimeBought = DateTime.Today,
+                ExpiryDate = new DateTime(2018, 01, 28),
+                MeasureType = "pcs"
+            });
+            list.Add(new Product
+            {
+                Name = "Banana",
+                Amount = 10,
+                DateTimeBought = DateTime.Today,
+                ExpiryDate = new DateTime(2018, 01, 28),
+                MeasureType = "pcs"
+            });
+            return View(list);
+        }
+    }
+}
---
+++ IKitchen/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IKitchen.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public DateTime DateTimeBought { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string MeasureType { get; set; }
+
+    }
+}
