f10271b233f110e1aeef52aab17aec18a5623283
Farida Akhtyamova <farida.akhtyamova@gmail.com>
2018-01-28 20:50:53 +0300

    2 files

---
+++ IKitchen/Models/Meal.cs
@@ -0,0 +1,27 @@
+﻿using IKitchen.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IKitchen.Models
+{
+    public class Meal
+    {
+        public Meal()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Display(AutoGenerateField = false)]
+        public int ID { get; set; }
+        [Display(Name = "Meal Name")]
+        public string MealName { get; set; }
+        public MealTypes MealType { get; set; }
+        public int ProductID { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+
+    public enum MealTypes { Breakfast, Lunch, Dinner, Snack, Dessert }
+}
